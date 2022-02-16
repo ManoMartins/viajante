3214e58a6b6e70798740cfccd2ba31cd960243dd
Luiz Henrique <7henrique18@gmail.com>
2022-02-16 20:11:15 -0300

    :artist_palette: Setup up theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,24 @@
+import Head from "next/head";
+import { GlobalStyles } from "../styles/global";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="Descubra locais incríveis para se visitar em cidades maravilhosas de Santa Catarina."
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/styles/global.ts
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    // brand
+    --orange: ##F25D27;
+    --blue: #123952;
+
+    // texts
+    --title: ##123952;
+    --text: #617480;
+    --support: ##A0ACB3
+
+    // shapes
+    --shape: ##FFFFFF;
+    --shape_dark: #DCE2E6;
+
+    // others
+    --green: #51B853;
+    --blue_low: #DDE9F0;
+
+    // background
+    --background: #DCE2E6;
+
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    font-family: 'Roboto', sans-serif;
+    background: var(--background);
+    color: var(--text);
+  }
+`;
