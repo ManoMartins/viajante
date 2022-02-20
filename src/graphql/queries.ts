4c1a1c9ed31bc74425672647473c3af45da5b3d6
Luiz Henrique <7henrique18@gmail.com>
2022-02-20 14:58:23 -0300

    :gear: Increase fake cities daata

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -12,3 +12,16 @@ export const GET_ALL_CITIES = gql`
     }
   }
 `;
+
+export const GET_FIRST_10_CITIES = gql`
+  query getFirst10Cities {
+    allCities(page: 1, perPage: 10) {
+      id
+      name
+      description
+      thumbnail
+      places_count
+      slug
+    }
+  }
+`;
