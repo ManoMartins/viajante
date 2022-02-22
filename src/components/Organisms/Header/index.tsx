604285959fd0bdd86e5a16e5c8c8adae267b4f1d
Luiz Henrique <7henrique18@gmail.com>
2022-02-22 14:00:38 -0300

    :atom: Header component variants

--- src/components/Organisms/Header/index.tsx
+++ src/components/Organisms/Header/index.tsx
@@ -6,28 +6,32 @@ import { ButtonLink } from "../../Atoms/ButtonLink";
 export type HeaderProps = {
   headerGap?: string;
   variant?: "default" | "transparent";
+  hasSearch?: boolean;
 };
 
 export const Header = (props: HeaderProps) => {
-  const { headerGap = "40px", variant = "default" } = props;
+  const { headerGap = "40px", variant = "default", hasSearch } = props;
 
   return (
     <S.Header headerGap={headerGap} variant={variant}>
-      <Link href="/">
-        <a data-aos="fade-right" data-aos-duration="500">
-          <Image
-            src="/logo.svg"
-            width="126px"
-            height="26px"
-            alt="Logo Traveler"
-          />
-        </a>
-      </Link>
-      <ButtonLink
-        label="Acesso restrito"
-        href="/login"
-        onClick={() => alert("teste")}
-      />
+      <S.Content>
+        <Link href="/">
+          <a data-aos="fade-right" data-aos-duration="500">
+            <Image
+              src="/logo.svg"
+              width="126px"
+              height="26px"
+              alt="Logo Traveler"
+            />
+          </a>
+        </Link>
+        {hasSearch && <h3>🔍 Search</h3>}
+        <ButtonLink
+          label="Acesso restrito"
+          href="/login"
+          onClick={() => alert("teste")}
+        />
+      </S.Content>
     </S.Header>
   );
 };
--- src/components/Organisms/Header/styles.ts
+++ src/components/Organisms/Header/styles.ts
@@ -1,14 +1,29 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Header = styled.header<{
   headerGap: string;
   variant: "default" | "transparent";
 }>`
   width: 100%;
+  height: 5rem;
+
+  margin-bottom: ${({ headerGap }) => headerGap};
+
+  ${({ variant, theme }) =>
+    variant === "default" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.shape};
+      margin: 0 auto;
+      border: 1px solid ${({ theme }) => theme.colors.shape_dark};
+    `}
+`;
+
+export const Content = styled.div`
+  max-width: 1180px;
+  margin: 0 auto;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 5rem;
-  margin-bottom: ${({ headerGap }) => headerGap};
   padding: 0 1rem;
+  height: 100%;
 `;
